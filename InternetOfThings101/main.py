1ccd3711e1a198d9a4514c7e4138cf8e31cd6b22
Antonio <myjobgdl.gmail.com>
2016-07-14 16:35:47 +0000

    nternet of Things 101: 5.2.1 Sensors: Project: Data Collection
    Implement a function to collect data, data will be based on system-wide
    network I/O statistics using psutil python library, this will simulate
    our sensor in case no physical sensor is available
    Signed-off-by: Antonio <myjobgdl.gmail.com>

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python
 
+import psutil
+import signal
+import sys
 import time
 
+def functionDataSensor():
+   netdata = psutil.net_io_counters()
+   data = netdata.packets_sent + netdata.packets_recv
+   return data
+
+def functionSignalHandler(signal, frame):
+   sys.exit(0)
+
 if __name__ == '__main__':
+
+   signal.signal(signal.SIGINT, functionSignalHandler)
+   
    while True:
       print "Hello Internet of Things 101"
+      print "Data Sensor: %s " % functionDataSensor()
       time.sleep(5)
 
 # End of File
